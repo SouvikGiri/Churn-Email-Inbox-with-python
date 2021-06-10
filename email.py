57f850328b92fb989b9a0b9e714c4b80a6a88fa6
Souvik Giri <74680826+SouvikGiri@users.noreply.github.com>
2021-06-10 23:55:02 +0530

    Update email.py

--- email.py
+++ email.py
@@ -1,5 +1,3 @@
-# Churn-Email-Inbox-with-python
-#In this project, we will use Python to access the data from files and process it to achieve certain tasks. we will explore the MBox email dataset, and use Python to count lines, headers, subject lines by emails and domains. 
 
 def number_of_lines():
     fhand = open('/cxldata/datasets/project/mbox-short.txt')
